e8a4b39c5c23c17fbe6a051de2f4cad5f7d03dff
ren995 <2898492876@qq.com>
2020-02-22 15:54:43 +0800

    create project

---
+++ src/main/java/org/song/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package org.song.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+     @ResponseBody
+     @RequestMapping("/Hello")
+    public String HelloWorld(){
+        return "HelloWorld";
+     }
+}
